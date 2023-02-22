271745ece10c144f5fdd6853a34a9f69b8f55ebe
TPeleWL <116174979+TPeleWL@users.noreply.github.com>
2023-02-22 15:08:58 +0100

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -27,9 +27,8 @@ export default defineUserConfig<DefaultThemeOptions, WebpackBundlerOptions>({
     ],
     sidebar: [
       { text: '🔤 Lexicon', link: '/lexicon' },
-      { text: '📢 Overview', collapsible: false, children: [
+      { text: '📢 Overview', collapsible: true, children: [
         { text: 'Introduction', link: '/overview/#introduction' },
-        { text: 'Meet the DAM Platform', link: '/overview/#meet-the-dam-platform' },
         { text: 'Features and Benefits ', link: '/overview/#features-and-benefits ' }
       ]},      { text: '📱 Mobile application', link: '/mobile-app' },
       { text: '💻 Back Office', collapsible: false, children: [
